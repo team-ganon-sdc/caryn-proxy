0606b18f3805cbd3ec8df9daf69daedf5701d98c
Caryn Shu <40576975+carynshu@users.noreply.github.com>
2020-04-16 18:28:01 -0700

    connect to loader.io

--- server/index.js
+++ server/index.js
@@ -7,4 +7,8 @@ const port = 3000;
 // app.use(express.static('public'))
 app.use(express.static(path.join(__dirname, '/../public')));
 
+app.get('/loaderio-536aa6f241c3ba0c9409d4050ec8e10f.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, '../loaderio-536aa6f241c3ba0c9409d4050ec8e10f.txt'));
+});
+
 app.listen(port, () => console.log(`Proxy server running on port ${port}!`));
